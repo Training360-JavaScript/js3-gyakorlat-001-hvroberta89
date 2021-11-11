8cbabbb5f7731f3f3d6066a27737c61f7917d684
hvroberta89 <veber.roberta@gmail.com>
2021-11-11 09:02:26 +0100

    completed tasks

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,11 @@
 
-
-
+const HU = {
+  date: (date) => new Intl.DateTimeFormat('hu-HU').format(date),
+  curreny:(curreny) => new Intl.NumberFormat('hu-HU',{style:'currency',currency: 'HUF'}).format(curreny),
+  list: (list) => new Intl.ListFormat('hu-HU', { style:'long', type:'conjunction' }).format(list)
+}
 
 
 
 // export default HU
+export default HU;
